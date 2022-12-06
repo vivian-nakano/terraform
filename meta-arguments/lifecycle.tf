51e56b22010cb0015e0d6581c7723b3d51522b7c
vivian-nakano <vrnakano@gmail.com>
2022-12-05 22:13:26 -0300

    insert teorical and practical concepts about lifecycle meta argument

---
+++ meta-arguments/lifecycle.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "lifecycle" {
+  bucket = "vivian-lifecycle"
+
+  lifecycle {
+    create_before_destroy = true
+    #prevent_destroy = true
+    #ignore_changes = [tags,]
+  }
+}
